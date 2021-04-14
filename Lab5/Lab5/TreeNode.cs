1b2dd2660b6d6e95c11d224a09c75c86d4a56ffa
Sorp <danvuk11@gmail.com>
2021-04-14 10:27:26 +0300

    tree implementation

---
+++ Lab5/Lab5/TreeNode.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Lab5
+{
+    public delegate void TreeVisitor<T>(T nodeData);
+    public class TreeNode<T>
+    {
+        private T data;
+        private LinkedList<TreeNode<T>> children;
+
+        public TreeNode(T data)
+        {
+            this.data = data;
+            children = new LinkedList<TreeNode<T>>();
+        }
+
+        public void AddChild(T data)
+        {
+            children.AddFirst(new TreeNode<T>(data));
+        }
+
+        public TreeNode<T> GetChild(int i)
+        {
+            foreach (TreeNode<T> n in children)
+                if (--i == 0)
+                    return n;
+            return null;
+        }
+
+        public void Traverse(TreeNode<T> node, TreeVisitor<T> visitor)
+        {
+            visitor(node.data);
+            foreach (TreeNode<T> kid in node.children)
+                Traverse(kid, visitor);
+        }
+    }
+}
