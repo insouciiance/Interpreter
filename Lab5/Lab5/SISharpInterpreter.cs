2f15e0fd17687f5f8985be26f8482d031a87ce35
Eternity <nikkv.ottf@gmail.com>
2021-04-16 15:02:54 +0300

    added SiSharpInterpreter.cs

---
+++ Lab5/Lab5/SISharpInterpreter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interpreter.Collections;
+
+namespace Lab5
+{
+    internal class SISharpInterpreter
+    {
+        private Hashtable<string, double> _variables;
+
+        public void Execute(string[] lines)
+        {
+
+        }
+    }
+}
