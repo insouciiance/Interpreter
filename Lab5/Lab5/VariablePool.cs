7aa7b4d6d8e1dca74241e3446b79ba43b1848b73
Sorp <danvuk11@gmail.com>
2021-04-18 09:52:37 +0300

    add variable pool

---
+++ Lab5/Lab5/VariablePool.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+
+namespace Lab5.Commands
+{
+    public class VariablePool
+    {
+        private Hashtable pool;
+
+        public static VariablePool Instance = new VariablePool();
+
+        private VariablePool()
+        {
+            pool = new Hashtable();
+        }
+
+        public void SetValue(string varName,object? value)
+        {
+            if (pool.Contains(varName))
+                pool[varName] = value;
+            else
+                pool.Add(varName,value);
+        }
+        
+        public void SetValue<T>(string varName,T value)
+        {
+            if (pool.Contains(varName))
+                pool[varName] = value;
+            else
+                pool.Add(varName,value);
+        }
+
+        public object? GetValue(string varName)
+        {
+            return pool[varName];
+        }
+
+        public T GetValue<T>(string varName)
+        {
+            object value = pool[varName];
+            if (value is T castedVal)
+                return castedVal;
+
+            Console.WriteLine($"Cannot cast var:{varName} into target type:{typeof(T)} ");
+            return default;
+        }
+    }
+}
