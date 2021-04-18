565546d9306ddc843bfe901cb13656ac7659d2cb
Sorp <danvuk11@gmail.com>
2021-04-18 09:53:17 +0300

    add basic command assign variable

---
+++ Lab5/Lab5/Commands/AssignVariable.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab5.Commands
+{
+    public class AssignVariable<T> : IVisitable,IParent
+    {
+
+        private string name;
+        private T value;
+
+        public AssignVariable(string name,T value)
+        {
+            this.value = value;
+            this.name = name;
+        }
+
+        public IVisitable Next { get; set; }
+        
+        public void Visit()
+        {
+            VariablePool.Instance.SetValue(name,value);
+        }
+
+    }
+}
