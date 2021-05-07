351b8e1bfee7b2b5a737efbcac0c25b4e7a3be59
Sorp <danvuk11@gmail.com>
2021-05-08 00:12:01 +0300

    hot fix

--- Lab5/Lab5/NodeBuilders/IfStatementNodeBuilder.cs
+++ Lab5/Lab5/NodeBuilders/IfStatementNodeBuilder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lab5.SyntaxNodes;
 
 namespace Lab5
 {
@@ -58,19 +59,20 @@ namespace Lab5
         ITraversable INodeBuilder2.Build()
         {
             string[] splitStatement = _line.Split(
-                new [] { "if(", "){", "}else{", "}" },
+                new [] { "if", "then", "else" },
                 StringSplitOptions.RemoveEmptyEntries);
 
             string statementBody = splitStatement[0];
             string trueBlock = splitStatement[1];
 
-            ITraversable statementNode = new(NodeType.IfStatement, null);
+            SyntaxTreeNode statementNode = new(NodeType.IfStatement, null);
 
-            ITraversable conditionNode = new 
+            SyntaxTreeNode conditionNode = new(NodeType.Condition, null);
             conditionNode.AddChild(new SyntaxTreeNodeBuilder(statementBody).Build());
+            ConditionNode conditionNode = new ConditionNode(new Sy);
 
-            ITraversable trueNode = new(NodeType.ConditionTrue, null);
-            string[] trueBlockLines = trueBlock.Split('|');
+            SyntaxTreeNode trueNode = new(NodeType.ConditionTrue, null);
+            string[] trueBlockLines = trueBlock.Split("->");
 
             foreach (string line in trueBlockLines)
             {
@@ -83,8 +85,16 @@ namespace Lab5
             if (splitStatement.Length <= 2) return statementNode;
 
             string elseBlock = splitStatement[2];
+            string[] elseBlockLines = elseBlock.Split("->");
+
             SyntaxTreeNode falseNode = new(NodeType.ConditionFalse, null);
             falseNode.AddChild(new SyntaxTreeNodeBuilder(elseBlock).Build());
+
+            foreach (string line in elseBlockLines)
+            {
+                falseNode.AddChild(new SyntaxTreeNodeBuilder(line).Build());
+            }
+
             statementNode.AddChild(falseNode);
 
             return statementNode;
