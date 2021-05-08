397b1766a2a9f705c49586fa0c4f21302f2f2c15
Nikita Kovalenko <nikkv.ottf@gmail.com>
2021-05-08 10:44:10 +0300

    added for statement node builder

--- Lab5/Lab5/NodeBuilders/ForStatementNodeBuilder.cs
+++ Lab5/Lab5/NodeBuilders/ForStatementNodeBuilder.cs
@@ -8,9 +8,22 @@ namespace Lab5.NodeBuilders
 {
     internal class ForStatementNodeBuilder : INodeBuilder
     {
+        private string _line;
+
+        public ForStatementNodeBuilder(string line) => _line = line;
+
         public ITraversable Build()
         {
-            return null;
+            string[] splitFor = _line.Split(new string[]{"for(", "){", "}"}, StringSplitOptions.RemoveEmptyEntries);
+            string[] innerConditions = splitFor[0].Split("", StringSplitOptions.RemoveEmptyEntries);
+
+            ITraversable firstInner = new SyntaxTreeNodeBuilder(innerConditions[0]).Build();
+            ITraversable secondInner = new SyntaxTreeNodeBuilder(innerConditions[1]).Build();
+            ITraversable thirdInner = new SyntaxTreeNodeBuilder(innerConditions[2]).Build();
+
+            ITraversable body = new SyntaxTreeNodeBuilder(splitFor[1]).Build();
+
+            return new ForStatementNode(firstInner, secondInner, thirdInner, body);
         }
     }
 }
