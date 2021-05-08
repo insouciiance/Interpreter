b9f9ae8631b01dadab35ce7c481c2188ec3abb28
Nikita Kovalenko <nikkv.ottf@gmail.com>
2021-05-08 10:35:50 +0300

    added empty for statement builder

---
+++ Lab5/Lab5/NodeBuilders/ForStatementNodeBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.NodeBuilders
+{
+    internal class ForStatementNodeBuilder : INodeBuilder
+    {
+        public ITraversable Build()
+        {
+            return null;
+        }
+    }
+}
